c9a94e6985aa672958b2d583357d43ff27d1542a
enoch85 <enoch85@gmail.com>
2016-04-02 05:20:46 +0200

    remove unwanted text

--- debian/instruction.sh
+++ debian/instruction.sh
@@ -13,17 +13,10 @@ cat << INST2
 | If you have never done this before you can follow the complete        |
 | installation instructions here: https://goo.gl/3FYtz6                 |
 |                                                                       |
-| You can also set the ownCloud update process as a cronjob.            |
-| This is made through the built in script in this VM that              |
-| updates ownCloud, set secure permissions, and then                    |
-| logs the successful update in /var/log/cronjobs_success.log           |
-| Here is a guide on how to set it up:                                  |
-| https://www.techandme.se/set-automatic-owncloud-updates/		|
-|									|
 | You can schedule the ownCloud update process using a cron job.        |
 | This is done using a script built into this VM that automatically     |
-| updates ownCloud, sets secure permissions, and logs the               |
-| successful update to /var/log/cronjobs_success.log                    |
+| updates ownCloud, sets secure permissions, and logs the successful    |
+| update to /var/log/cronjobs_success.log                    		|
 | Detailed instructions for setting this up can be found here:          |
 | https://www.techandme.se/set-automatic-owncloud-updates/              |
 |                                                                       |
