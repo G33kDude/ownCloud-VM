2a3a4a24a5bd617dc49e9a9bd794ed10f68bf618
GeekDude <G33kDude@users.noreply.github.com>
2016-04-01 22:45:28 -0400

    Update instruction.sh

--- beta/instruction.sh
+++ beta/instruction.sh
@@ -10,14 +10,14 @@ echo -e "|"  "\e[32mTo run the startup script, just type the sudoer password\e[0
 echo -e "|"  "\e[32mThe sudoer password is 'owncloud', or the PW you chose during install\e[0m |"
 cat << INST2
 |                                                                       |
-| If you never done this before you can follow the complete             |
-| install instructions: https://goo.gl/3FYtz6                           |
+| If you have never done this before you can follow the complete        |
+| installation instructions here: https://goo.gl/3FYtz6                 |
 |                                                                       |
-| You can also set the ownCloud update process as a cron job.           |
-| This is made through the built in script in this VM that              |
-| updates ownCloud, set secure permissions, and then                    |
-| logs the successful update in /var/log/cronjobs_success.log           |
-| Here is a guide on how to set it up:                                  |
+| You can schedule the ownCloud update process using a cron job.        |
+| This is done using a script built into this VM that automatically     |
+| updates ownCloud, sets secure permissions, and logs the               |
+| successful update to /var/log/cronjobs_success.log                    |
+| Detailed instructions for setting this up can be found here:          |
 | https://www.techandme.se/set-automatic-owncloud-updates/              |
 |                                                                       |
 |  ####################### Tech and Me - 2016 ########################  |
